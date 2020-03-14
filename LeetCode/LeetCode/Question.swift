78f7421f75b18aac428f5724d8fe7a24b79fb16b
gavin <tao.tao5@ymm56.com>
2020-03-14 16:24:12 +0800

    有效的数独

--- LeetCode/LeetCode/Question.swift
+++ LeetCode/LeetCode/Question.swift
@@ -1382,3 +1382,70 @@ class QuestionThirtyfive {
         return index;
     }
 }
+
+/*
+ 有效的数独
+ */
+
+class QuestionThirtysix {
+    func isValidSudoku(_ board: [[Character]]) -> Bool {
+        return rowsValid(board) && colsValid(board) && subBoardValid(board);
+    }
+    
+    private func rowsValid(_ board:[[Character]]) -> Bool {
+        var existDigits = Set<Character>();
+        for i in 0..<board.count {
+            existDigits.removeAll();
+            for j in 0..<board[0].count {
+                if !isDigitValid(board[i][j], &existDigits) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private func colsValid(_ board:[[Character]]) ->Bool{
+        var existDigits = Set<Character>();
+        for i in 0..<board[0].count {
+            for j in 0..<board.count {
+                if !isDigitValid(board[j][i], &existDigits) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private func subBoardValid(_ board:[[Character]]) ->Bool{
+        var existingDigits = Set<Character>()
+        
+        for i in stride(from: 0, to: board.count, by: 3) {
+            for j in stride(from: 0, to: board[0].count, by: 3) {
+                existingDigits.removeAll()
+                
+                for m in i..<i + 3 {
+                    for n in j..<j + 3 {
+                        if !isDigitValid(board[m][n], &existingDigits) {
+                            return false
+                        }
+                    }
+                }
+            }
+        }
+        return true
+    }
+    
+    private func isDigitValid(_ digit: Character, _ set: inout Set<Character>) -> Bool {
+        if digit == "." {
+            return true
+        }
+        
+        if set.contains(digit) {
+            return false
+        } else {
+            set.insert(digit)
+            return true
+        }
+    }
+}
