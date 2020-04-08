06a2b8a0c8486e1c6d9d00231f9f90a460070916
gavin <tao.tao5@ymm56.com>
2020-04-08 16:40:55 +0800

    41:缺失的第一个正数

--- LeetCode/LeetCode/Question.swift
+++ LeetCode/LeetCode/Question.swift
@@ -1552,3 +1552,19 @@ class QuestionFourty {
     }
     
 }
+
+/*
+ 缺失的第一个正数
+ */
+class QuestionFourtyOne {
+    func firstMissingPositive(_ nums: [Int]) -> Int {
+        let set = Set(nums);
+        for i in 0..<nums.count {
+            if !set.contains(i+1) {
+                return i + 1;
+            }
+        }
+        return nums.count + 1;
+    }
+    
+}
