f2595877412265863ecb57c8852f9b1a8c3a8ba1
gavin <tao.tao5@ymm56.com>
2020-06-17 18:55:54 +0800

    加一

--- LeetCode/LeetCode/Question.swift
+++ LeetCode/LeetCode/Question.swift
@@ -2432,3 +2432,25 @@ class QuestionSixtyFour {
         
     }
 }
+
+
+/*
+ 加一
+ */
+class QuestionSixtySix{
+    
+    func plusOne(_ digits: [Int]) -> [Int] {
+        var digits = digits;
+        var index = digits.count - 1;
+        while index >= 0 {
+            if digits[index] < 9 {
+                digits[index] += 1;
+                return digits;
+            }
+            digits[index] = 0;
+            index -= 1;
+        }
+        digits.insert(1, at: 0);
+        return digits;
+    }
+}
