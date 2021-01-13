99188bd078b25ad879e75b341630c5d3f6ab69eb
gavin <tao.tao5@ymm56.com>
2021-01-13 18:50:56 +0800

    合并两个有序数组。

--- LeetCode/LeetCode/Question.swift
+++ LeetCode/LeetCode/Question.swift
@@ -3098,3 +3098,37 @@ class QuestionEightySix {
     }
     
 }
+
+/**
+ 扰乱字符串
+ */
+
+class QuestionEightySeven {
+    func isScramble(_ s1: String, _ s2: String) -> Bool {
+            
+        return true;
+    }
+}
+
+
+/**
+ 合并两个有序数组
+ */
+
+class QuestionEightyEight {
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+
+        var i = m - 1, j = n - 1
+        
+        while i >= 0 || j >= 0 {
+            if j < 0 || (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[i + j + 1] = nums1[i]
+                i -= 1
+            } else {
+                nums1[i + j + 1] = nums2[j]
+                j -= 1
+            }
+        }
+        
+    }
+}
